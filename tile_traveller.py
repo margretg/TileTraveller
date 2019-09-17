a66f3721396b0ef25c9b98cb141dfb460649a797
Margret Gudmundsdottir <margret@margud.com>
2019-09-17 14:14:29 +0000

    movecharacter

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,10 +1,70 @@
+user_input('You can travel: ')
 
 
-def movecharacter():
-    pass
+def movecharacter(position):
+    if position == 1:
+        if user_input == 'N':
+            return position + 1
+        else:
+            pass
+    if position == 2:
+        if user_input == 'N':
+            return position + 1
+        elif user_input == 'E':
+            return position + 3
+        elif user_input == 'S':
+            return position - 1
+        else:
+            pass
+
+    if position == 3:
+        if user_input == 'E':
+            return position + 3
+        elif user_input == 'S':
+            return position - 1
+        else:
+            pass
+    if position == 4:
+        if user_input == 'N':
+            return position + 1
+        else:
+            pass
+    if position == 5:
+        if user_input == 'W':
+            return position - 3
+        elif user_input == 'S':
+            return position - 1
+        else:
+            pass
+    if position == 6:
+        if user_input == 'W':
+            return position - 3
+        elif user_input == 'E':
+            return position + 3
+        else:
+            pass
+    if position == 7:
+        if user_input == 'N':
+            return position + 1
+        else:
+            pass
+    if position == 8:
+        if user_input == 'N':
+            return position + 1
+        elif user_input == 'S':
+            return position - 1
+        else:
+            pass
+    if position == 9:
+        if user_input == 'W':
+            return position - 3
+        elif user_input == 'S':
+            return position - 1
+        else:
+            pass
 
 
-def legalmoves(n,s,w,e):
+def legalmoves(n, s, w, e):
     print("You can travel", end=' ')
     if n == 1:
         print("(N)orth")
@@ -37,5 +97,3 @@ while True:
         pass
     if position == 9:
         break
-
-
